3320da2e54851f44d1f69ad67bbaa3bac02cbd68
Lawrence F MacFadyen <larry.macfadyen@gmail.com>
2015-02-26 16:41:09 -0500

    Initial Commit

---
+++ AutomatedScrollingMessage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutomatedScrollingMessage
+//
+//  Created by Lawrence F MacFadyen on 2015-02-26.
+//  Copyright (c) 2015 LawrenceM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutomatedScrollingMessage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutomatedScrollingMessage
+//
+//  Created by Lawrence F MacFadyen on 2015-02-26.
+//  Copyright (c) 2015 LawrenceM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
